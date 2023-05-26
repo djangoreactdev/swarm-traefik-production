1f6e59d897c2fb25983baa62f137c1b86f197877
djangoreactdev <djangoreactdev@gmail.com>
2023-05-26 23:16:33 +0000

    add build config

--- script.groovy
+++ script.groovy
@@ -3,8 +3,9 @@
 def deployApp() {
     echo 'deploying the application...'
     sh 'pwd && ls'
-    sh 'docker compose -f production.yml build'
+    sh 'docker compose -f production-build.yml build'
     sh 'docker stack rm production || true'
+    sh 'docker network create production_default'
     sh 'docker stack deploy -c production.yml production'
 } 
 
