4dd998956c6c38aca26f8d7583281297ef3e7827
djangoreactdev <djangoreactdev@gmail.com>
2023-05-26 22:28:33 +0000

    asd

--- script.groovy
+++ script.groovy
@@ -2,9 +2,10 @@
 
 def deployApp() {
     echo 'deploying the application...'
-    sh 'docker compose -f production.yml build'
-    sh 'docker stack rm production || true'
-    sh 'docker stack deploy -c production.yml production'
+    sh 'pwd'
+    // sh 'docker compose -f production.yml build'
+    // sh 'docker stack rm production || true'
+    // sh 'docker stack deploy -c production.yml production'
 } 
 
 return this
