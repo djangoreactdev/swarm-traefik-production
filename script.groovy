cf3cfd31cb6d3195f1fbf99ef3b0ec5d9e57df75
djangoreactdev <djangoreactdev@gmail.com>
2023-05-27 18:06:07 +0000

    sadfasdf

--- script.groovy
+++ script.groovy
@@ -5,6 +5,7 @@ def deployApp() {
     sh 'pwd && ls'
     sh 'docker compose -f production-build.yml build'
     sh 'docker stack rm production || true'
+    sh 'docker network rm production_default || true'
     sh 'docker network create production_default || true'
     sh 'docker stack deploy -c production.yml production'
 } 
