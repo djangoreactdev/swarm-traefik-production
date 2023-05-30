17bec35f3cab191b5a63b203faf5a54aca3e570f
djangoreactdev <djangoreactdev@gmail.com>
2023-05-30 20:59:18 +0000

    chenge network

--- script.groovy
+++ script.groovy
@@ -6,7 +6,8 @@ def deployApp() {
     sh 'docker compose -f production-build.yml build'
     sh 'docker stack rm production || true'
     sh 'docker network rm production_default || true'
-    sh 'docker network create production_default || true'
+    sh 'docker network create -d bridge production_default || true'
+    sh 'docker network create -d overlay production_default || true'
     sh 'docker stack deploy -c production.yml production'
 } 
 
