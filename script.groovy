4e6e8ae49793a7876f3a7fd63849b39b97a0b281
djangoreactdev <djangoreactdev@gmail.com>
2023-05-30 10:01:08 +0000

    add latest

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def deployApp() {
     sh 'docker compose -f production-build.yml build'
     sh 'docker stack rm production || true'
     sh 'docker network rm production_default || true'
-    sh 'docker network create production_default || true'
+    // sh 'docker network create production_default || true'
     sh 'docker stack deploy -c production.yml production'
 } 
 
